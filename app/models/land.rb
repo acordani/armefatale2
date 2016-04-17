4cb179fb3d3573aedba0208ca817abf40be91b09
ac <alcordani@gmail.com>
2016-04-17 09:07:41 +0200

    validation model Land

--- app/models/land.rb
+++ app/models/land.rb
@@ -1,2 +1,9 @@
 class Land < ActiveRecord::Base
+	validates :number, presence:true
+  	validates :surface, presence: true
+ 	validates :address, presence: true
+  	validates :neighborhood, presence: true
+  	validates :city, presence: true
+  	validates :citysearch, presence: true
+  	validates :file, presence:true
 end
