28fb838f554ac0442a98d8cab1584ca5650ad5bb
ac <alcordani@gmail.com>
2016-04-30 19:22:39 +0200

    signin sign up by modal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # For modal Inscription
+  helper_method :resource_name, :resource, :devise_mapping
+  #
 
   	def after_sign_in_path_for(resource)
   		lands_path
